64849a6a888156b40b69d09a11d51143f5bdc055
Vishwanath Yadav <Vsy081999@gmail.com>
2024-04-04 00:36:56 +0530

    first commit

---
+++ src/test/java/zJenkins_demo/Test_2.java
@@ -0,0 +1,19 @@
+package zJenkins_demo;
+
+import org.testng.annotations.Test;
+
+public class Test_2 {
+
+	@Test
+
+	public void name() {
+		System.out.println("name_regression");
+	}
+
+	@Test
+
+	public void address() {
+		System.out.println("address_regression");
+	}
+
+}
