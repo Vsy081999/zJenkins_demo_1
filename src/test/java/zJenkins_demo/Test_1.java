7f094521470aeb1ba70718342250baa929e8e231
Vishwanath Yadav <Vsy081999@gmail.com>
2024-04-04 18:26:19 +0530

    new commit

---
+++ src/test/java/zJenkins_demo/Test_1.java
@@ -0,0 +1,19 @@
+package zJenkins_demo;
+
+import org.testng.annotations.Test;
+
+public class Test_1 {
+
+	@Test
+
+	public void name() {
+		System.out.println("name_sanity");
+	}
+
+	@Test
+
+	public void address() {
+		System.out.println("address_sanity");
+	}
+
+}
